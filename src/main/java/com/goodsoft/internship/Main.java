2040dd9f3f86df838dcf316636899c4f122b528a
NataliyaPrimudraya <nataliaprimudraa@gmail.com>
2025-01-09 15:09:23 +0300

    reformat code

--- src/main/java/com/goodsoft/internship/Main.java
+++ src/main/java/com/goodsoft/internship/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println("Задание 1");
 
         HashSet<String> strings = new HashSet<>();
-        for (int i=1; i<6; i++)
+        for (int i = 1; i < 6; i++)
             strings.add("Строка " + i);
 
         System.out.println("Вывод с применением foreach:");
@@ -18,24 +18,23 @@ public class Main {
 
         Iterator<String> iterator = strings.iterator();
         System.out.println("Вывод с применением итератора:");
-        while (iterator.hasNext())
-            System.out.print(iterator.next()+"; ");
+        while (iterator.hasNext()) System.out.print(iterator.next() + "; ");
         System.out.println();
 
         //===================================================
 
         HashSet<User> users = new HashSet<>();
-        for (int i=1; i<6; i++)
-            users.add(new User(i, "Nickname "+i));
+        for (int i = 1; i < 6; i++)
+            users.add(new User(i, "Nickname " + i));
 
         System.out.println("Множество пользовательских классов:");
-        users.forEach(u->System.out.print(u + "; "));
+        users.forEach(u -> System.out.print(u + "; "));
         System.out.println();
 
         users.add(new User(1, "Nickname 1"));
 
         System.out.println("Мн-во после попытки добавления дубликата:");
-        users.forEach(u->System.out.print(u + "; "));
+        users.forEach(u -> System.out.print(u + "; "));
         System.out.println();
 
         //===================================================
@@ -43,7 +42,7 @@ public class Main {
         System.out.println("Задание 2");
 
         ArrayList<String> strArray = new ArrayList<>();
-        for (int i=1; i<4; i++)
+        for (int i = 1; i < 4; i++)
             strArray.add("Строка " + i);
 
         System.out.println("Вывод с применением for, аналогичного foreach:");
@@ -51,24 +50,24 @@ public class Main {
             System.out.print(s + "; ");
 
         System.out.println("\nВывод с добавлением элемента на 2-ую позицию после вывода 3-го:");
-        for (int i=0; i<strArray.size();i++) {
+        for (int i = 0; i < strArray.size(); i++) {
             System.out.print(strArray.get(i) + "; ");
-            if (i==2) strArray.add(1, "new");
+            if (i == 2) strArray.add(1, "new");
         }
 
         System.out.println("\nРезультат:");
-        strArray.forEach(s->System.out.print(s + "; "));
+        strArray.forEach(s -> System.out.print(s + "; "));
         /*Размер strArray и позиция последнего элемента
         увеличивыаются на единицу, последний элемент выводится повторно*/
 
         System.out.println("\nВывод с удалением 2-го элемента после вывода 3-го:");
-        for (int i=0; i<strArray.size();i++) {
+        for (int i = 0; i < strArray.size(); i++) {
             System.out.print(strArray.get(i) + "; ");
-            if (i==2) strArray.remove(1);
+            if (i == 2) strArray.remove(1);
         }
 
         System.out.println("\nРезультат:");
-        strArray.forEach(s->System.out.print(s + "; "));
+        strArray.forEach(s -> System.out.print(s + "; "));
         /*Размер strArray уменьшается на единицу, цикл
          не доходит до последнего элемента*/
 
@@ -78,32 +77,32 @@ public class Main {
         System.out.println("Работа с словарём HashMap<String, String>");
 
         HashMap<String, String> strMap = new HashMap<>();
-        for (int i=1; i<4; i++)
+        for (int i = 1; i < 4; i++)
             strMap.put("Ключ " + i, "Строка " + i);
 
         System.out.println("Вывод с применением foreach:");
-        strMap.forEach((k,v)->System.out.print("("+k+": "+v+"); "));
+        strMap.forEach((k, v) -> System.out.print("(" + k + ": " + v + "); "));
 
         strMap.put("Ключ 3", "new");
 
         System.out.println("\nВывод с применением for, аналогичного foreach:");
         for (Map.Entry<String, String> entry : strMap.entrySet())
-            System.out.print("("+entry.getKey()+": "+entry.getValue()+"); ");
+            System.out.print("(" + entry.getKey() + ": " + entry.getValue() + "); ");
 
 
         System.out.println("\nРабота с словарём HashMap<User, String>");
         HashMap<User, String> userMap = new HashMap<>();
-        for (int i=1; i<4; i++)
+        for (int i = 1; i < 4; i++)
             userMap.put(new User(i, "Ключ " + i), "Строка " + i);
 
         System.out.println("Вывод с применением foreach:");
-        userMap.forEach((k,v)->System.out.print("("+k+": "+v+"); "));
+        userMap.forEach((k, v) -> System.out.print("(" + k + ": " + v + "); "));
 
         userMap.put(new User(3, "Ключ 3"), "new");
 
         System.out.println("\nВывод с применением for, аналогичного foreach:");
         for (Map.Entry<User, String> entry : userMap.entrySet())
-            System.out.print("("+entry.getKey()+": "+entry.getValue()+"); ");
+            System.out.print("(" + entry.getKey() + ": " + entry.getValue() + "); ");
 
         //===================================================
 
@@ -113,14 +112,14 @@ public class Main {
         strSorted.add("А");
         strSorted.add("П");
         System.out.println("Отсортированное множество строк:");
-        strSorted.forEach(s->System.out.print(s + "; "));
+        strSorted.forEach(s -> System.out.print(s + "; "));
 
         TreeSet<User> usersSorted = new TreeSet<>();
-        usersSorted.add(new User(1,"Н"));
-        usersSorted.add(new User(2,"А"));
-        usersSorted.add(new User(3,"П"));
+        usersSorted.add(new User(1, "Н"));
+        usersSorted.add(new User(2, "А"));
+        usersSorted.add(new User(3, "П"));
         System.out.println("\nОтсортированное мн-во пользовательских классов:");
-        usersSorted.forEach(u->System.out.print(u + "; "));
+        usersSorted.forEach(u -> System.out.print(u + "; "));
 
         //===================================================
 
@@ -131,14 +130,14 @@ public class Main {
         strSortedMap.put("А", "Ю");
         strSortedMap.put("П", "Н");
         System.out.println("Отсортированный словарь с ключами-строками:");
-        strSortedMap.forEach((k,v)->System.out.print("("+k+": "+v+"); "));
+        strSortedMap.forEach((k, v) -> System.out.print("(" + k + ": " + v + "); "));
 
         TreeMap<User, String> usersSortedMap = new TreeMap<>();
-        usersSortedMap.put(new User(1,"Н"), "П");
-        usersSortedMap.put(new User(2,"А"), "Ю");
-        usersSortedMap.put(new User(3,"П"), "Н");
+        usersSortedMap.put(new User(1, "Н"), "П");
+        usersSortedMap.put(new User(2, "А"), "Ю");
+        usersSortedMap.put(new User(3, "П"), "Н");
         System.out.println("\nОтсортированный словарь с ключами-классами:");
-        usersSortedMap.forEach((k,v)->System.out.print("("+k+": "+v+"); "));
+        usersSortedMap.forEach((k, v) -> System.out.print("(" + k + ": " + v + "); "));
 
     }
 }
--- src/main/java/com/goodsoft/internship/User.java
+++ src/main/java/com/goodsoft/internship/User.java
@@ -5,33 +5,36 @@ import java.util.Objects;
 public class User implements Comparable<User> {
     private int id;
     private String name;
-    public User(int id, String name){
-        this.id=id;
-        this.name=name;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
     @Override
     public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+        return "User{" + "id=" + id + ", name='" + name + '\'' + '}';
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (obj == this) { return true; }
+        if (obj == this) {
+            return true;
+        }
 
-        if (obj == null || obj.getClass() != this.getClass()) { return false; }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
 
         User user = (User) obj;
-        return Objects.equals(id, user.id) &&
-                Objects.equals(name, user.name);
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name);
     }
+
     @Override
     public int hashCode() {
         return Objects.hash(id, name);
     }
+
     @Override
     public int compareTo(User user) {
         return name.compareTo(user.name);
